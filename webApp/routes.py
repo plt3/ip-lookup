eaeab1d49aba176c363ad387f469bc7a312deb22
Paul Taylor <pltorigami@gmail.com>
2021-01-07 16:28:00 -0500

    renamed utils.py and cleaned up routes.py

--- webApp/routes.py
+++ webApp/routes.py
@@ -6,7 +6,7 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from webApp import app, templates
 from webApp.errors import ASNPrefixError, IPDetailsError
-from webApp.utils import getAllInfo
+from webApp.queryApi import getAllInfo
 from webApp.validate import validateIp
 
 
@@ -16,7 +16,7 @@ async def home(request: Request, ip: Optional[str] = None):
 
     :request (fastapi.Request): Required to return any response template
     :ip (str): IPv4 or IPv6 address as a string to get information about. If not
-    included, will show basica IP address search form
+    included, will show basic IP address search form
     :returns: fastapi.templating.Jinja2Templates.TemplateResponse for HTML page of
     web app
 
@@ -48,27 +48,6 @@ async def home(request: Request, ip: Optional[str] = None):
         "ipDetails": ipDetails,
         "asnPrefixes": asnPrefixes,
     }
-
-    # import json
-
-    # if isIpValid:
-    #     # with open("ipDetails.json", "w") as f:
-    #     with open("ipDetails.json") as f:
-    #         # json.dump(ipDetails, f, indent=2)
-    #         ipDetails = json.load(f)
-    #     # with open("asnPrefixes.json", "w") as f:
-    #     with open("asnPrefixes.json") as f:
-    #         # json.dump(asnPrefixes, f, indent=2)
-    #         asnPrefixes = json.load(f)
-
-    # infoDict = {
-    #     "request": request,
-    #     "ip": cleanIp,
-    #     "error": error,
-    #     "isIpValid": isIpValid,
-    #     "ipDetails": ipDetails,
-    #     "asnPrefixes": asnPrefixes,
-    # }
 
     return templates.TemplateResponse("home.html", infoDict)
 
